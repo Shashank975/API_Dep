c6a6675237fa419e5b30020759382cee6f30dea6
Shashank Chhoker <shashanksaad07@gmail.com>
2025-01-18 23:43:35 +0530

    added flask_cors and frontend section

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify,request
 # import db_cnxn
+from flask_cors import CORS
 from db import db
 import json
-app = Flask(__name__)
 
+
+app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
