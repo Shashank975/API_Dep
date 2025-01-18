c63fd803a14b2bf4238cde8c0972c7673330d2ed
Shashank Chhoker <shashanksaad07@gmail.com>
2025-01-18 23:28:26 +0530

    added dummy db file to check if it works without odbc driver or not

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify,request
-import db_cnxn
+# import db_cnxn
+from db import db
 import json
 app = Flask(__name__)
 
@@ -11,5 +12,6 @@ def home():
 
 @app.route('/api/dep_data')
 def Department_dataAPI():
-    data = db_cnxn.department_data()
+    # data = db_cnxn.department_data()
+    data = db
     return jsonify(data)
---
+++ db.py
@@ -0,0 +1,98 @@
+db = [
+    {
+      "DepartmentID": 1,
+      "GroupName": "Research and Development",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Engineering"
+    },
+    {
+      "DepartmentID": 2,
+      "GroupName": "Research and Development",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Tool Design"
+    },
+    {
+      "DepartmentID": 3,
+      "GroupName": "Sales and Marketing",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Sales"
+    },
+    {
+      "DepartmentID": 4,
+      "GroupName": "Sales and Marketing",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Marketing"
+    },
+    {
+      "DepartmentID": 5,
+      "GroupName": "Inventory Management",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Purchasing"
+    },
+    {
+      "DepartmentID": 6,
+      "GroupName": "Research and Development",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Research and Development"
+    },
+    {
+      "DepartmentID": 7,
+      "GroupName": "Manufacturing",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Production"
+    },
+    {
+      "DepartmentID": 8,
+      "GroupName": "Manufacturing",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Production Control"
+    },
+    {
+      "DepartmentID": 9,
+      "GroupName": "Executive General and Administration",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Human Resources"
+    },
+    {
+      "DepartmentID": 10,
+      "GroupName": "Executive General and Administration",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Finance"
+    },
+    {
+      "DepartmentID": 11,
+      "GroupName": "Executive General and Administration",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Information Services"
+    },
+    {
+      "DepartmentID": 12,
+      "GroupName": "Quality Assurance",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Document Control"
+    },
+    {
+      "DepartmentID": 13,
+      "GroupName": "Quality Assurance",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Quality Assurance"
+    },
+    {
+      "DepartmentID": 14,
+      "GroupName": "Executive General and Administration",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Facilities and Maintenance"
+    },
+    {
+      "DepartmentID": 15,
+      "GroupName": "Inventory Management",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Shipping and Receiving"
+    },
+    {
+      "DepartmentID": 16,
+      "GroupName": "Executive General and Administration",
+      "ModifiedDate": "Wed, 30 Apr 2008 00:00:00 GMT",
+      "Name": "Executive"
+    }
+  ]
