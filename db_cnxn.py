10d43d79940b2f63185fb9654132534e6895df7e
Shashank Chhoker <shashanksaad07@gmail.com>
2025-01-18 22:58:28 +0530

    added timeout in db conection

--- db_cnxn.py
+++ db_cnxn.py
@@ -12,7 +12,9 @@ cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};\
                       SERVER='+server+';\
                       DATABASE='+database+';\
                       UID='+username+';\
-                      PWD='+ password)
+                      PWD='+password+';\
+                      Trusted_Connection=yes;'
+                      'Timeout=60')
 
 cursor = cnxn.cursor()
 
